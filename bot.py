a2d55a18e8b816956206cb6b47b0801de9dfe7a9
WicahyaD <104063345+WicahyaD@users.noreply.github.com>
2022-09-06 14:25:49 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,27 +1,43 @@
-import streamlit as st
-from telebot import *
-import pafy
-import os
+from flask import Flask
+from googlesearch import search
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
 
-st.write("Hai...")
 
-api = "5451328552:AAHQrBiYiiDNJgfwjpdm9c_eK3FraKUZinA"
-bot = TeleBot(api)
+app = Flask(__name__)
 
-@bot.message_handler(commands=["mp4"])
+@app.route("/", methods=["POST"])
 
-def video(message):
-    bot.send_message(message.chat.id, "On Progressing...")
-    url = pafy.new(message.text.replace("/mp4", ""))
-    bot.send_message(message.chat.id, url.title)
-    bot.send_message(message.chat.id, "Please Wait...")
-    hasil = url.getbest()
-    hasil.download()
+# chatbot logic
+def bot():
 
-    for i in os.listdir():
-        if i.endswith(".mp4"):
-            print(i)
-            bot.send_message(message.chat.id, open(i, "rb"))
-            os.remove(i)
+	# user input
+	user_msg = request.values.get('Body', '').lower()
 
-bot.polling()
+	# creating object of MessagingResponse
+	response = MessagingResponse()
+
+	# User Query
+	q = user_msg + "geeksforgeeks.org"
+
+	# list to store urls
+	result = []
+
+	# searching and storing urls
+	for i in search(q, tld='co.in', num=6, stop=6, pause=2):
+		result.append(i)
+
+	# displaying result
+	msg = response.message(f"--- Result for '{user_msg}' are ---")
+
+	msg = response.message(result[0])
+	msg = response.message(result[1])
+	msg = response.message(result[2])
+	msg = response.message(result[3])
+	msg = response.message(result[4])
+
+	return str(response)
+
+
+if __name__ == "__main__":
+	app.run()
